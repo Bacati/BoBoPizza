97e403b6c2b8342f07a94adbb8967deb967d8de9
Bacati <lucaslievre2001@gmail.com>
2024-09-11 12:29:10 +0200

    Ajout suppression modification de la carte

--- src/main/java/fr/eni/Pizza/app/controller/PizzaController.java
+++ src/main/java/fr/eni/Pizza/app/controller/PizzaController.java
@@ -2,17 +2,29 @@ package fr.eni.Pizza.app.controller;
 
 
 import fr.eni.Pizza.app.bll.IProduitManager;
+import fr.eni.Pizza.app.bll.MySQL.TypeProduitManager;
+import fr.eni.Pizza.app.bo.Produit;
+import fr.eni.Pizza.app.bo.TypeProduit;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@SessionAttributes({"typeSession","membreSession"})
+
 public class PizzaController {
 
+    private final TypeProduitManager typeProduitManager;
     IProduitManager produitManager;
 
-    public PizzaController(IProduitManager produitManager) {
+    public PizzaController(IProduitManager produitManager, TypeProduitManager typeProduitManager) {
         this.produitManager = produitManager;
+        this.typeProduitManager = typeProduitManager;
     }
 
     //clés model "produits", "pizza-produits", "boisson-produits"
@@ -34,4 +46,44 @@ public class PizzaController {
 
         return "modifCarte";
     }
+    @GetMapping("/detail")
+    public String afficherUnProduit(@RequestParam("idProduit") long id, Model model) {
+        Produit p = this.produitManager.getProduitById(id);
+        List<TypeProduit> typeSession = typeProduitManager.getAllTypeProduits();
+        model.addAttribute("typeSession", typeSession);
+        model.addAttribute("produit", p);
+        return "carteDetail";
+    }
+
+    @GetMapping("/creer")
+    public String creer(Model model) {
+            Produit p = new Produit();
+            model.addAttribute("produit", p);
+            return "carteNew";
+
+    }
+    @PostMapping("/creer")
+    public String modifCarte(@Valid @ModelAttribute("produit") Produit produit, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "carteDetail";
+        }
+        produitManager.saveProduit(produit);
+        return "redirect:/laCarte";
+    }
+
+    @PostMapping("/delete")
+    public String deleteProduit(@Valid @ModelAttribute("produit") Produit produit, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "carteDetail";
+        }
+        produitManager.deleteProduitById(produit.getId());
+        System.out.println("delete"+ produit.getId());
+        return "redirect:/laCarte";
+    }
+
+    @ModelAttribute("typeSession")
+    public List<TypeProduit> chargerTypeSession(){
+        return typeProduitManager.getAllTypeProduits();
+    }
 }
+
